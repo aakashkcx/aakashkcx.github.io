86450bec36902a12eff902976dda6364030950f6
Aakash Kc <aakashkcx@gmail.com>
2025-02-04 03:21:48 +0000

    add prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: { parser: "astro" },
+    },
+  ],
+};
+
+export default config;
