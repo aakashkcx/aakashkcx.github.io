3397fbfa057b71ee555574e418268944f87815c8
Aakash Kc <aakashkcx@gmail.com>
2025-02-20 04:22:33 +0000

    fix portfolio rss

--- src/pages/portfolio/rss.xml.ts
+++ src/pages/portfolio/rss.xml.ts
@@ -5,18 +5,18 @@ import { getCollection } from "astro:content";
 export async function GET(context: APIContext) {
   const projects = (await getCollection("projects"))
     .filter((project) => project.data.published)
-    .toSorted((a, b) => b.data.created.valueOf() - a.data.created.valueOf());
+    .toSorted((a, b) => b.data.started.valueOf() - a.data.started.valueOf());
 
   return rss({
     title: "Aakash Kc / Portfolio",
     description: "Hi there, I'm Aakash! Welcome to my portfolio!",
     site: context.site!,
     items: projects.map((project) => {
-      const { title, description, created, tags } = project.data;
+      const { title, description, started, tags } = project.data;
       return {
         link: `/portfolio/project/${project.id}`,
         title,
-        pubDate: created,
+        pubDate: started,
         description,
         categories: tags,
       };
