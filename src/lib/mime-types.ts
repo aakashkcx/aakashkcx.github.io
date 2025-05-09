f0fb4116599076f33778a7076d261aa0b872bfb8
Aakash Kc <aakashkcx@gmail.com>
2025-05-09 17:04:42 +0100

    fix: add astro image mime types - fixes incorrect svg mime type

---
+++ src/lib/mime-types.ts
@@ -0,0 +1,10 @@
+export const imageMimeTypes = {
+  jpeg: "image/jpeg",
+  jpg: "image/jpeg",
+  png: "image/png",
+  tiff: "image/tiff",
+  webp: "image/webp",
+  gif: "image/gif",
+  svg: "image/svg+xml",
+  avif: "image/avif",
+} as const;
